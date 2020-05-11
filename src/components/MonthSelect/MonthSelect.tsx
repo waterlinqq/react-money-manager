340c03b4ceb6e0f06e2bdf4d16be0462f2905e17
waterlinqq <a10311036@gmail.com>
2020-05-11 14:22:33 +0800

    feat(MonthSelect): component and style

---
+++ src/components/MonthSelect/MonthSelect.tsx
@@ -0,0 +1,39 @@
+import React, { useState, FC } from 'react'
+import { DatePickerView } from 'antd-mobile'
+import dayjs from 'dayjs'
+
+import classes from './MonthSelect.module.scss'
+
+interface IProps {
+  month: Date
+  monthChanged(month: Date): void
+}
+const MonthSelect: FC<IProps> = ({ month, monthChanged }) => {
+  const [val, setVal] = useState(month)
+  const [isShow, setIsShow] = useState(false)
+  const confirmHandler = () => {
+    setIsShow(false)
+    monthChanged(val)
+  }
+  const isHidden = { display: isShow ? 'block' : 'none' }
+  return (
+    <div className={classes.MonthSelect}>
+      <div onClick={() => setIsShow(!isShow)}>
+        <span>{dayjs(month).format('YYYY年M月')}</span>
+        <span className={classes.Dropdown} />
+      </div>
+      <div className={classes.Mask} style={isHidden} onClick={confirmHandler} />
+      <div className={classes.Monthpicker}>
+        {isShow ? (
+          <DatePickerView
+            mode={'month'}
+            value={val}
+            onChange={(value) => setVal(value)}
+          />
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default MonthSelect
