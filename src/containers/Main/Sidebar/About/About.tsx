142fda2a5e036c950d11d6d4135799940d45742e
waterlinqq <a10311036@gmail.com>
2020-05-12 17:15:03 +0800

    feat(Sidebar): About and Review components

---
+++ src/containers/Main/Sidebar/About/About.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const About = () => (
+  <div>
+    <h3>Ｍoney Manager</h3>
+    <p>版本號 1.0.0</p>
+  </div>
+)
+export default About
---
+++ src/containers/Main/Sidebar/Review/Review.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Review = () => (
+  <div onClick={() => (window.location.href = 'https://github.com/waterlinqq')}>
+    <h2>請給我顆Star吧!</h2>
+    <p style={{ color: '#666' }}>前往我的github</p>
+    {Array.from({ length: 5 }).map((_, i) => (
+      <img
+        src={require('../../../../images/system/評價我們.svg')}
+        key={i}
+        alt="星星"
+        style={{
+          height: '25px',
+          padding: '6px',
+          color: 'rgba(255, 217, 0, 0.876)',
+        }}
+      />
+    ))}
+  </div>
+)
+export default Review
